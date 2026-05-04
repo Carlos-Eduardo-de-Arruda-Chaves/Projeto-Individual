862cb0463cdf52e773a88444aaf52d2c2a6dcce1
Carlos-Eduardo-de-Arruda-Chaves <carlos.chaves@sptech.school>
2026-05-04 17:21:50 -0300

    Adicionando inserção de kruang, equipe e data de inicio de treino ao usuário

--- src/controllers/graduacaoController.js
+++ src/controllers/graduacaoController.js
@@ -65,6 +65,7 @@ function cadastrar(req, res) {
     var kruang = req.body.kruangServer;
     var equipe = req.body.equipeServer;
     var data = req.body.dataServer;
+    var email = req.body.emailServer;
     // var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
@@ -79,7 +80,7 @@ function cadastrar(req, res) {
     } */ else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(kruang, equipe, data/*, fkEmpresa*/)
+        usuarioModel.cadastrar(kruang, equipe, data, email/*, fkEmpresa*/)
             .then(
                 function (resultado) {
                     res.json(resultado);
